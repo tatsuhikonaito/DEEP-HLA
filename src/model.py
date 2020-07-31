adfae18050f21535f4871cb0b3f29a52eb826beb
tatsuhikonaito <67576643+tatsuhikonaito@users.noreply.github.com>
2020-07-31 14:18:31 +0900

    Update model.py

--- src/model.py
+++ src/model.py
@@ -27,7 +27,7 @@ class SharedNet(nn.Module):
 
     def forward(self, x, mask_input, mask_conv1, mask_conv2):
         x = x.transpose(1, 2)  # A hack to handle Conv1d input
-        # Input can be randomly collapsed with (0, 0) vector to learn missing-values during training. 
+        # Input can be randomly corrupted with (0, 0) vector to learn missing-values during training. 
         if self.training and self.input_collapse:
             if mask_input is None:
                 mask_input = torch.bernoulli(x.data.new(x.data.size()).fill_(random.uniform(0.8, 1)))
